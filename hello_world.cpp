cdce6ead6ed551dc669e40d93bcffad226fef9ed
PetrBrick <petrbrick@gmail.com>
2021-05-07 21:18:12 +0300

    added some new

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-
 #include <string>
-
 int main() {
-         std::string name;
-         std::cin >> name;
+         std::string name; // user name
+         std::cin >> name;// input user name
          std::cout << "Hello world "<< name << std::endl;
          return 0;
 }
+
