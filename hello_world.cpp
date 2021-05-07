7d63d375dc081369d5bff3b241d0175464d2e0f2
PetrBrick <petrbrick@gmail.com>
2021-05-07 22:24:07 +0300

    fixed

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <string>
-int
-main()
-{
-  std::string name; // user name
-  std::cin >> name; // input user name
-  std::cout << "Hello world " << name << std::endl;
-  return 0;
+int main() {
+         std::string name; // user name
+         std::cin >> s;// input user name
+         std::cout << "Hello world "<< name << std::endl;
+         return 0;
 }
