230bf90cfca420e9b7e59649d0f9eed7ff6c469f
PetrBrick <petrbrick@gmail.com>
2021-05-07 21:39:16 +0300

    Changed something here

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <string>
-int main() {
-         std::string name; // user name
-         std::cin >> name;// input user name
-         std::cout << "Hello world "<< name << std::endl;
-         return 0;
+int
+main()
+{
+  std::string name; // user name
+  std::cin >> name; // input user name
+  std::cout << "Hello world " << name << std::endl;
+  return 0;
 }
-
